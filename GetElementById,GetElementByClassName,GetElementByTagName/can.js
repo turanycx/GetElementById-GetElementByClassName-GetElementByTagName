468d90e686a8a1074c6a5eb777bd6438822dc561
turanycx <turanycc@gmail.com>
2024-09-23 12:56:30 +0300

    Add files via upload

---
+++ GetElementById,GetElementByClassName,GetElementByTagName/can.js
@@ -0,0 +1,8 @@
+const deneme=document.getElementById('can');
+console.log(deneme);
+
+const hatalar=document.getElementsByClassName('error');
+console.log(hatalar[0]);
+
+const pEtiketleri=document.getElementsByTagName('p');
+console.log(pEtiketleri[1]);
